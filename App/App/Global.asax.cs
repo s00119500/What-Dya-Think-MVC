506d427ba3d332b6a491b8bef531ea51223957f4
Thomas <s00119500@mail.itsligo.ie>
2015-03-26 14:19:18 +0000

    Edited Configuration Files
    added membership to web.config
    added WebSecurity to Global.asax

--- App/App/Global.asax.cs
+++ App/App/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebMatrix.WebData;
 
 namespace App
 {
@@ -16,6 +17,9 @@ namespace App
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            /// added web security to app 
+            /// this allows app to access data table at app start
+            WebSecurity.InitializeDatabaseConnection("myConnectionString","UserProfile","UserID","UserName",true);
         }
     }
 }
