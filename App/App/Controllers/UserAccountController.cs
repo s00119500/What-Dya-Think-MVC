799f5d1e48aa4cf062291ab1c305896d2c55b29a
Thomas <s00119500@mail.itsligo.ie>
2015-03-26 14:07:16 +0000

    user account
    added User Account Controller
    added User Login View
    added Web Matrix assembly

---
+++ App/App/Controllers/UserAccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        // GET: UserAccount
+        public ActionResult Index()
+        {
+            /// not used
+            return View();
+        }
+
+        /// user wants to log on 
+        public ActionResult Login() { return View(); }
+
+        /// user wants to register
+        public ActionResult Register() { return View(); }
+    }
+}
